c1978bc3e4439d7d6f81a44794863f01afada671
dmls <danmlsv@gmail.com>
2020-12-30 11:44:14 -0500

    Add logo

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import logo from '../assets/images/logo-white.png';
 
 class Header extends React.Component {
   constructor(props) {
@@ -24,12 +25,16 @@ class Header extends React.Component {
     return (
       <Container fluid className="bg-blueDark position-fixed py-4">
         <Row className="mx-5">
-          <Col>
-            <h2 className="font-poppins">Web Development</h2>
+          <Col sm={12} md={6}>
+            <Row>
+              <Col sm={2}>
+                <img class="img-fluid" src={logo} alt="DM Logo" />
+              </Col>
+            </Row>
           </Col>
           
-          <Col>
-            <div className="float-right mt-2">
+          <Col sm={12} md={6}>
+            <div className="float-right mt-4">
               {this.navItems.map(function(item, index) {
                 return (
                   <a className="ml-3 text-white font-weight-bold" href={item.url}>{item.label}</a>
