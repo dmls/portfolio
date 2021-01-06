26e724e1f4d8ca7539ebe6464ad5c90718091a5f
dmls <danmlsv@gmail.com>
2021-01-06 14:37:17 -0500

    Remove unused import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/scss/main.scss';
 import { Helmet } from 'react-helmet';
-import { Container } from 'react-bootstrap';
 import Header from './Components/Header';
 import Intro from './Components/Intro';
 import Work from './Components/Work';
