aaafcc635bc04ea815b069b7d4ebc20c3c4e59db
dmls <danmlsv@gmail.com>
2021-01-12 12:18:11 -0500

    Fix header styles

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 import logo from '../assets/images/logo-white.png';
 
 class Header extends React.Component {
@@ -27,17 +27,21 @@ class Header extends React.Component {
         <Row className="mx-5">
           <Col sm={12} md={6}>
             <Row>
-              <Col sm={2}>
-                <img class="img-fluid" src={logo} alt="DM Logo" />
+              <Col className="d-none d-md-block" sm={2}>
+                <Image fluid src={logo} alt="DM Logo" />
+              </Col>
+
+              <Col className="d-sm-block d-md-none text-center">
+                <Image className="w-25" src={logo} alt="DM Logo" />
               </Col>
             </Row>
           </Col>
           
           <Col sm={12} md={6}>
-            <div className="float-right mt-4">
+            <div className="float-sm-none float-md-right mt-4 text-center">
               {this.navItems.map(function(item, index) {
                 return (
-                  <a className="ml-3 ml-md-5 text-white font-weight-bold" href={item.url}>{item.label}</a>
+                  <a className={(index === 0 ? '' : 'ml-3 ml-md-5') + ' text-white font-weight-bold'} href={item.url}>{item.label}</a>
                 );
               })}
             </div>
